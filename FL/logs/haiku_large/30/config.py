4ee08796291e9a3f9fa7871111688a3e67faf797
Vishnu Asutosh Dasu <vishnu98dasu@gmail.com>
2024-10-01 16:04:31 -0400

    upload FL logs

---
+++ FL/logs/haiku_large/30/config.py
@@ -0,0 +1,36 @@
+# ML PARAMETERS
+MODEL_NAME = "gpt2-large"
+BATCH_SIZE = 8
+LEARNING_RATE = 5e-5
+DUPLICATE_RATE = 0.3 # between 0 and 1
+MAX_SEQ_LEN = 100
+EPOCHS = 1
+DATASET = "Haiku"
+# DATASET = "IMDB"
+
+EOS_TOKEN = "<|endoftext|>"
+BOS_TOKEN = "<|startoftext|>"
+PAD_TOKEN = "<|pad|>"
+
+
+TEST_RATIO = 0.2
+SEED = 123
+
+# EP-MPD PARAMETERS
+# If True, then the EP-MPD deduplication will be applied to client datasets before FL training.
+# This will result in 0% duplication and will override the effects of DUPLICATE_RATE.
+# Setting this to true will effectively result in DUPLICATE_RATE = 0.0
+USE_EPMPD = True
+TYPE = 1 # Type can be 1 or 2
+
+# OTHER PARAMETERS
+MODEL_PATH = "trained_models"
+MODEL_CACHE = "models_cache"
+
+# FL PARAMETERS
+CLIENTS = 10
+ROUNDS = 5
+
+
+# Other params
+CACHE_PATH = "/scratch/vad5173"
